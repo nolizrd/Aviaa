1f19b14d947b6506a5c609e10c2fbc842aa94b9e
nolizrd <evilunicorn669@gmail.com>
2023-11-10 02:07:14 +0800

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -25,30 +25,36 @@ class MainActivity : AppCompatActivity() {
         btn_date1=findViewById(R.id.btn_date1)
 
         val myCalendar= Calendar.getInstance()
+        val myCalendar1= Calendar.getInstance()
+
         val datePicker=DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
 
             myCalendar.set(Calendar.YEAR, year)
             myCalendar.set(Calendar.MONTH, month)
             myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateLable(myCalendar)
+            updateLabel(myCalendar, picked_date)
         }
-        btn_date.setOnClickListener{
-            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show()
 
+        val datePicker1 = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+            myCalendar1.set(Calendar.YEAR, year)
+            myCalendar1.set(Calendar.MONTH, month)
+            myCalendar1.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(myCalendar1, picked_date1)
+        }
+
+        btn_date.setOnClickListener{
+            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show()
         }
         btn_date1.setOnClickListener{
-            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show()
-
+            DatePickerDialog(this, datePicker1, myCalendar1.get(Calendar.YEAR), myCalendar1.get(Calendar.MONTH), myCalendar1.get(Calendar.DAY_OF_MONTH)).show()
         }
 
     }
-    private fun updateLable(myCalendar: Calendar)
-    {
+    private fun updateLabel(myCalendar: Calendar, picked_date: TextView){
         val myFormat="dd-MM-yyyy"
         val sdf=SimpleDateFormat(myFormat, Locale.UK)
 
-        picked_date.setText(sdf.format(myCalendar.time))
-        picked_date1.setText(sdf.format(myCalendar.time))
+        picked_date.text=sdf.format(myCalendar.time)
     }
-
 }
